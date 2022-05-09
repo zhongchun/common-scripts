500da20c41a35eb7adb90add0c11f41058a27c92
bermaker <495571751@qq.com>
2022-05-09 09:42:28 +0800

    Update import_to_mysql.sh

--- shell/mysql/import_to_mysql.sh
+++ shell/mysql/import_to_mysql.sh
@@ -6,11 +6,14 @@ if [ $# -lt 1 ]; then
 fi
 
 file="$1"
+host=$2
+port=$3
+user=$4
+password=$5
+database=$6
 
 MYSQL='/home/map/.jumbo/bin/mysql'
 
-cmd="${MYSQL} -h10.38.41.58 -P6266 -uui36qx66266 -pPL37ojsbVyTvzBofl -Dcongest_analysis -e \"load data local infile '${file}' replace  into table guiding_screen_render fields terminated by'\t' lines terminated by '\n' (id,cityCode,configData,screenId,createTime,updatetime,renderTime)\""
-
-# cmd="${MYSQL}  -uroot  -pMhxzKhl   -hcp01-map-da02.cp01.baidu.com   -P3306  -Dyanpan -e \"load data local infile '${file}' replace  into table baidu_event_base_data fields terminated by'\t' lines terminated by '\n' (id,bdeventid,platform,type,provincecode,citycode,districtname,starttime,offlinetime,location,direction,name,roadtype,lane,detail,content,ugcpro,ugccon,pic,uname,source,eventsource,valid,inmajor,createtime,updatetime)\""
+cmd="${MYSQL} -h$host -P$port -u$user -p$password -D$database -e \"load data local infile '${file}' replace  into table guiding_screen_render fields terminated by'\t' lines terminated by '\n' (id,cityCode,configData,screenId,createTime,updatetime,renderTime)\""
 
 eval "${cmd}"
