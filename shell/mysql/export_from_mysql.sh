000c1f8bd686318d77f7781e58a1bd1428f1a3fa
bermaker <495571751@qq.com>
2022-05-09 09:40:58 +0800

    Update export_from_mysql.sh

--- shell/mysql/export_from_mysql.sh
+++ shell/mysql/export_from_mysql.sh
@@ -2,5 +2,9 @@
 
 mysql_bin='/home/map/.jumbo/bin/mysql'
 
-${mysql_bin} -h10.38.41.58 -P6266 -uui36qx66266 -pPL37ojsbVyTvzBofl -Dcongest_analysis -N -e "set character set utf8; select * from guiding_screen_render"  > guiding_screen_render.txt
-# ${mysql_bin} -uitcp_web_w -pnMpQTUgJlYhAC1y3 -h10.111.80.218 -P7045 -Dmap_itcp_web -N -e "set character set utf8; select * from guiding_screen_render"  > guiding_screen_render.txt
+host=$1
+port=$2
+user=$3
+password=$4
+database=$5
+${mysql_bin} -h$1 -P$2 -u$3 -p$4 -D$5 -N -e "set character set utf8; select * from guiding_screen_render"  > guiding_screen_render.txt
